8612991d557aeb59df43d2f9741f7f302d6311f7
MIJanda <mokiliisaacjanda@gmail.com>
2024-07-16 22:50:05 +0300

    chore: complete application set up
    - extended the tailwindcss styles
    - added shadcn-ui to the project

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
-      <h2 className="text-3xl underline">Home</h2>
+      <h2 className="text-white text-3xl underline">Home</h2>
+      <Button>Click Me</Button>
     </div>
   );
 }
